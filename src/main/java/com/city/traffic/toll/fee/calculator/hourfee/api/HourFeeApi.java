776cff6b6b0bf01430090ef9afe2914123e876cc
Khaled Mostafa <eng.khmostafa@gmail.com>
2023-10-06 14:28:25 +0300

    Enhance Logs And Change File Names - 2

--- src/main/java/com/city/traffic/toll/fee/calculator/hourfee/api/HourFeeApi.java
+++ src/main/java/com/city/traffic/toll/fee/calculator/hourfee/api/HourFeeApi.java
@@ -2,12 +2,9 @@ package com.city.traffic.toll.fee.calculator.hourfee.api;
 
 import com.city.traffic.toll.fee.calculator.common.exception.ApiResponse;
 import com.city.traffic.toll.fee.calculator.common.model.dto.PaginationDto;
-import com.city.traffic.toll.fee.calculator.freedays.model.payload.request.AddFreeDayRequest;
-import com.city.traffic.toll.fee.calculator.freedays.model.payload.response.FreeDayResponse;
-import com.city.traffic.toll.fee.calculator.hourfee.model.payload.request.AddHourFeeRequest;
+import com.city.traffic.toll.fee.calculator.hourfee.model.payload.request.HourFeePayload;
 import com.city.traffic.toll.fee.calculator.hourfee.model.payload.response.HourFeeResponse;
 import com.city.traffic.toll.fee.calculator.hourfee.service.HourFeeService;
-import com.city.traffic.toll.fee.calculator.user.model.enums.EmployeeRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,13 +17,13 @@ public class HourFeeApi {
     private final HourFeeService hourFeeService;
 
     @PostMapping
-    public ApiResponse<HourFeeResponse> add(@RequestHeader("email") String email, @RequestBody AddHourFeeRequest addHourFeeRequest){
-        return hourFeeService.add(email, addHourFeeRequest);
+    public ApiResponse<HourFeeResponse> add(@RequestHeader("email") String email, @RequestBody HourFeePayload hourFeePayload){
+        return hourFeeService.add(email, hourFeePayload);
     }
 
     @PutMapping("/{hourFeeId}")
-    public ApiResponse<HourFeeResponse> update(@RequestHeader("email") String email, @PathVariable("hourFeeId") Long id, @RequestBody AddHourFeeRequest addHourFeeRequest){
-        return hourFeeService.update(email, id, addHourFeeRequest);
+    public ApiResponse<HourFeeResponse> update(@RequestHeader("email") String email, @PathVariable("hourFeeId") Long id, @RequestBody HourFeePayload hourFeePayload){
+        return hourFeeService.update(email, id, hourFeePayload);
     }
 
     @DeleteMapping("/{hourFeeId}")
